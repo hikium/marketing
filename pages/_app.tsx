b2acc627bd3b0ce5be7c45816341b5a0833e9c1f
Noah S <82749151+noahlst@users.noreply.github.com>
2022-10-17 20:24:18 +1100

    Test with classic implementation, headers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 // Types
-import { ReactElement, useEffect } from "react";
+import type { ReactElement } from "react";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
 
@@ -11,8 +11,11 @@ import "@fontsource/public-sans/600.css";
 
 // Global providers
 import { MotionConfig, LazyMotion, domAnimation } from "framer-motion";
+
+import { useEffect } from "react";
 // import { useRouter } from "next/router";
-import { useAckee } from "use-ackee";
+// import { useAckee } from "use-ackee";
+import * as ackeeTracker from "ackee-tracker";
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactElement;
@@ -29,41 +32,25 @@ export default function Application({
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout ?? ((page) => page);
 
+  // const { pathname } = useRouter();
+  // useAckee(
+  //   "/",
+  //   {
+  //     server: "https://ackee.hikium.com",
+  //     domainId: "2e846c93-9ead-47d0-b8dc-1e9cc57d2520",
+  //   },
+  //   {
+  //     detailed: false,
+  //     ignoreLocalhost: true,
+  //     ignoreOwnVisits: true,
+  //   }
+  // );
   // Analytics
-  // const router = useRouter();
-  // useEffect(() => {
-  //   const handleRouteChange = (url: any) => {
-  //     useAckee(
-  //       url,
-  //       {
-  //         server: "https://ackee.hikium.com",
-  //         domainId: "2e846c93-9ead-47d0-b8dc-1e9cc57d2520",
-  //       },
-  //       {
-  //         detailed: false,
-  //         ignoreLocalhost: true,
-  //         ignoreOwnVisits: true,
-  //       }
-  //     );
-  //   };
-  //   router.events.on("routeChangeComplete", handleRouteChange);
-  //   return () => {
-  //     router.events.off("routeChangeComplete", handleRouteChange);
-  //   };
-  // }, [router.events]);
-
-  useAckee(
-    "/",
-    {
-      server: "https://ackee.hikium.com",
-      domainId: "2e846c93-9ead-47d0-b8dc-1e9cc57d2520",
-    },
-    {
-      detailed: false,
-      ignoreLocalhost: true,
-      ignoreOwnVisits: true,
-    }
-  );
+  useEffect(() => {
+    ackeeTracker
+      .create("https://ackee.hikium.com")
+      .record("2e846c93-9ead-47d0-b8dc-1e9cc57d2520");
+  });
 
   return (
     <MotionConfig reducedMotion="user">

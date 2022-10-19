dd9f95d455c38382b9122cb28e07925fa29ae864
Stylus <99179258+CoderStylus@users.noreply.github.com>
2022-10-19 12:41:33 +1100

    Added footer and continued work on layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,16 +4,22 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import BrandWordmark from "components/brand/BrandWordmark";
 import Link from "next/link";
 import ECIcon from "components/brand/ECIcon";
-import hcjlogo from "pages/img/html-css-js-logo-combo.png";
+import ExternalLink from "components/system/ExternalLink";
 
 const navigation = [
   { name: "Contact", href: "#" },
-  { name: "Legal", href: "#" },
+  { name: "Legal", href: "/legal" },
 ];
 
-export default function Homepage() {
+interface LayoutProps {
+  children: React.ReactNode;
+  manualMargin: boolean;
+}
+
+export default function Homepage({ children, manualMargin }: LayoutProps) {
   return (
     <div className="relative overflow-hidden bg-black">
+      <title>Hikium</title>
       <section>
         <div className="mx-auto max-w-7xl">
           <div className="relative z-10 bg-black pb-8 sm:pb-16 md:pb-20 lg:w-full lg:max-w-2xl lg:pb-28 xl:pb-32">
@@ -95,8 +101,8 @@ export default function Homepage() {
             <main className="mx-auto mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
               <div className="sm:text-center lg:text-left">
                 <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl md:content-center md: justify-center">
-                  <span className="block xl:inline text-white md:text-center">Bringing the </span>
-                  <span className="block text-brand xl:inline md:text-center">element of elegance </span>
+                  <span className="block xl:inline text-white sm:text-center">Bringing the </span>
+                  <span className="block text-brand xl:inline sm:text-center">element of elegance </span>
                   <span className="block xl:inline text-white md:text-center">to the web.</span>
                 </h1>
                 <p className="mt-3 text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0 flex gap-2 tracking-tight text-center">
@@ -124,8 +130,8 @@ export default function Homepage() {
         <h1 className="text-4xl font-bold tracking-tight text-white sm:text-5xl md:text-6xl text-center bg-black pt-20">
           No strings attatched.
         </h1>
-        <p className="text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl gap-2 tracking-tight text-center">
-          Hikium <ECIcon /> uses advanced website building technologies that don't come at a cost. All your work is completely yours. No watermarks, no pay-to-use. Simply build your website with Hikium <ECIcon /> and export your code to host it anywhere, on any platform. The best part? No sign up is required. Simply load up your favourite browser and start creating your own <span className="text-brand">Element of Elagance.</span>
+        <p className="text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl gap-2 text-center">
+          Hikium EC uses advanced website building technologies that don't come at a cost. All your work is completely yours. No watermarks, no pay-to-use. Simply build your website with Hikium EC and export your code to host it anywhere, on any platform. The best part? No sign up is required. Simply load up your favourite browser and start creating your own <span className="text-brand">Element of Elagence.</span>
         </p>
       </section>
 
@@ -133,10 +139,53 @@ export default function Homepage() {
         <h1 className="text-4xl font-bold tracking-tight text-white sm:text-5xl md:text-6xl text-center bg-black pt-20">
           Compile to the most popular web languages in the world.
         </h1>
-        <hcjlogo />
+        
 
         
       </section>
+
+      
+
+      <footer
+        className={`${
+          manualMargin && "px-5 lg:px-52"
+        } pb-10 flex flex-col space-y-5 lg:flex-row lg:space-x-20 lg:space-y-0 text-center align-center justify-center`}
+      >
+        <div className="flex flex-col space-y-1 text-sm">
+          <p className="text-gray-500">Follow our hike</p>
+          <ExternalLink
+            href="https://twitter.com/hikium"
+            className="text-gray-500 hover:text-white hover:cursor-pointer flex flex-row space-x-2"
+          >
+            Twitter @hikium
+          </ExternalLink>
+        </div>
+        <div className="flex flex-col space-y-1 text-sm">
+          <ExternalLink
+            href="https://github.com/hikium"
+            className="text-gray-500 hover:text-white hover:cursor-pointer flex flex-row space-x-2"
+          >
+            GitHub
+          </ExternalLink>
+        </div>
+        <div className="flex flex-col space-y-1 text-sm">
+          <Link href="/legal/privacy">
+            <a className="underline underline-offset-4 text-gray-500 hover:text-white hover:cursor-pointer">
+              Privacy
+            </a>
+          </Link>
+          <Link href="/legal">
+            <a className="underline underline-offset-4 text-gray-500 hover:text-white hover:cursor-pointer">
+              Terms and Legal
+            </a>
+          </Link>
+          <Link href="/legal/security">
+            <a className="underline underline-offset-4 text-gray-500 hover:text-white hover:cursor-pointer">
+              Responsible Disclosure
+            </a>
+          </Link>
+        </div>
+      </footer>
     </div>
   
   );

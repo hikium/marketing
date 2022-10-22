4c5c3a89c0194925c9969ebbf8be0a32502fa13a
Noah S <82749151+noahlst@users.noreply.github.com>
2022-10-22 19:48:48 +1100

    Add the brand collaboration card to Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,10 +7,25 @@ import Head from "next/head";
 // First party components
 import ECIcon from "components/brand/ECIcon";
 
+// Design
+import { IconArrowRight, IconBrandSpotify } from "@tabler/icons";
+import { m } from "framer-motion";
+
 // Layouts
 import Layout from "components/layouts/Layout";
+import Button from "components/system/Button";
+import SupplementaryText from "components/system/SupplementaryText";
+
+import { useState, Suspense } from "react";
+import { useInterval } from "hooks/useInterval";
 
 export default function Home() {
+  // Brand collaboration card animation
+  const [isHeadingGreyed, setHeadingGreyed] = useState(false);
+  const [showCultureHeading, setShowCultureHeading] = useState(false);
+  useInterval(() => setHeadingGreyed(true), 1200);
+  useInterval(() => setShowCultureHeading(true), 1750);
+
   return (
     <>
       <Head>
@@ -27,7 +42,17 @@ export default function Home() {
       </Head>
 
       <main className="flex flex-col space-y-48">
-        <section className="flex flex-col space-y-10 px-5 lg:px-52">
+        {/* Headline marketing - our brand collaborations */}
+        <section
+          className="flex flex-col justify-between shadow-sm bg-black/5 dark:bg-white/5 p-20 mx-5"
+          style={{
+            borderTopLeftRadius: "2em",
+            borderTopRightRadius: "2em",
+            borderBottomLeftRadius: "2em",
+            borderBottomRightRadius: "2em",
+            height: "75vh",
+          }}
+        >
           <div className="flex flex-col space-y-0">
             <div className="flex flex-row md:space-x-5">
               <h2
@@ -36,36 +61,108 @@ export default function Home() {
               >
                 The new Hikium
               </h2>
-              <div className="hidden md:flex md:items-center md:justify-center ">
+              <div className="hidden md:flex md:items-center md:justify-center">
                 <div className="w-20 h-20">
                   <ECIcon />
                 </div>
               </div>
               {/* Mobile */}
               <h2
-                style={{ fontSize: "5.5em" }}
+                style={{ fontSize: "2em" }}
                 className="leading-none md:hidden"
               >
                 The new Hikium EC
               </h2>
             </div>
-            <h2 style={{ fontSize: "5.5em" }} className="leading-none">
-              The elegance element
-            </h2>
-          </div>
-          <div className="flex flex-col space-y-2 md:flex-row md:space-x-10">
-            <p className="md:flex md:items-center md:justify-center">
-              Free and priceless website building
-            </p>
-            {/* <div className="w-1/4">
-              <Button>Open the App</Button>
-            </div> */}
-            <div className="w-fit">
-              <div className="bg-black/10 dark:bg-white/10 px-5 py-2 font-display uppercase text-sm rounded-2xl">
-                Coming Soon
-              </div>
+            <div className="flex flex-row md:space-x-5">
+              <h2
+                style={{ fontSize: "5.5em" }}
+                className="leading-none hidden md:flex"
+              >
+                Composes
+              </h2>
+              <Suspense
+                fallback={
+                  <h2
+                    style={{ fontSize: "5.5em" }}
+                    className="leading-none md:hidden"
+                  >
+                    Composes {showCultureHeading ? "culture" : "websites"}
+                  </h2>
+                }
+              >
+                {showCultureHeading ? (
+                  <m.h2
+                    initial={{ scale: 0 }}
+                    animate={{ scale: 1 }}
+                    exit={{ scale: 0 }}
+                    style={{ fontSize: "5.5em" }}
+                    className="leading-none hidden md:flex"
+                  >
+                    culture
+                  </m.h2>
+                ) : (
+                  <m.h2
+                    initial={{ scale: 1 }}
+                    animate={{ scale: 0 }}
+                    exit={{ scale: 1 }}
+                    transition={{ delay: 1.6 }}
+                    style={{
+                      fontSize: "5.5em",
+                      color: isHeadingGreyed ? "#9ca3af" : "inherit",
+                    }}
+                    className="leading-none hidden md:flex"
+                  >
+                    websites
+                  </m.h2>
+                )}
+              </Suspense>
+              {/* Mobile */}
+              <h2
+                style={{ fontSize: "2em" }}
+                className="leading-none md:hidden"
+              >
+                Composes {showCultureHeading ? "culture" : "websites"}
+              </h2>
             </div>
           </div>
+          <div className="flex flex-col space-y-5 md:flex-row md:justify-between">
+            <div className="flex flex-col md:flex-row space-y-2 md:space-x-5">
+              {/* Album cover */}
+              <div className="bg-black/25 dark:bg-white/25 h-36 w-36 rounded-2xl" />
+              <div className="flex justify-center items-end">
+                <Button icon={<IconBrandSpotify />}>Stream the Album</Button>
+              </div>
+            </div>
+            <div className="flex flex-col md:flex-row space-y-2 md:space-x-5 justify-center items-end">
+              <SupplementaryText className="flex justify-center items-center text-end">
+                Build your free website now
+              </SupplementaryText>
+              <Button icon={<IconArrowRight />} isDisabled>
+                Open the EC App
+              </Button>
+            </div>
+          </div>
+        </section>
+
+        {/* Product marketing will go here */}
+
+        {/* End of page call-to-action */}
+        <section className="flex flex-row justify-between mx-5 lg:mx-52">
+          <div className="flex flex-row space-x-5 justify-center items-center">
+            <div className="flex justify-center items-center">
+              <div className="w-6 h-6">
+                <ECIcon />
+              </div>
+            </div>
+            <div className="flex justify-center items-center">
+              <p>Hikium Experience Composer website builder</p>
+            </div>
+          </div>
+          {/* Replace with Open EC button when launched */}
+          <span className="bg-black/5 dark:bg-white/5 px-5 rounded-full flex justify-center items-center text-sm font-display uppercase">
+            Coming Soon
+          </span>
         </section>
         <section className="flex flex-col space-y-0 mx-5 lg:mx-52 text-sm text-gray-600 dark:text-gray-400 lg:w-1/3">
           <p>Early development version. Features may change before release.</p>

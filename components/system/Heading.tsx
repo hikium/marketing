d5641e5afc995035863a6acddf02f23f6b066395
Noah S <82749151+noahlst@users.noreply.github.com>
2022-11-05 14:49:18 +1100

    Add custom class support for custom Heading

--- components/system/Heading.tsx
+++ components/system/Heading.tsx
@@ -3,6 +3,7 @@ import { createElement } from "react";
 interface HeadingProps {
   children: React.ReactNode;
   as: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+  className?: string;
 }
 
 /**
@@ -10,9 +11,10 @@ interface HeadingProps {
  *
  * @beta
  */
-export default function Heading({ children, as }: HeadingProps) {
+export default function Heading({ children, as, className }: HeadingProps) {
+  // Allow for additional classes to be applied
+  const internalClassNames = `${className} text-5xl md:text-6xl lg:text-7xl`;
+
   // Return an element
-  return createElement(as, { className: "text-xl md:text-4xl lg:text-7xl" }, [
-    children,
-  ]);
+  return createElement(as, { className: internalClassNames }, [children]);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -153,7 +153,9 @@ export default function Home() {
 
         {/* Easy to use section */}
         <section>
-          <Heading as="h2">Test</Heading>
+          <Heading as="h2" className="text-red-400">
+            Test
+          </Heading>
         </section>
 
         {/* Code export */}

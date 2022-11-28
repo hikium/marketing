2ffdada3d01ce1ca773fe8420d0ab4c1828dd698
Noah S <82749151+noahlst@users.noreply.github.com>
2022-11-28 18:17:50 +1100

    Try a new CSP and allow origin

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
 
+const contentSecurityPolicy = `
+  default-src 'self' *.hikium.com *-hikium-marketing.netlify.app;
+  script-src 'self' *.hikium.com *-hikium-marketing.netlify.app;
+  child-src hikium.com;
+  style-src 'self';
+  font-src 'self';  
+`;
+
 const globalHeaders = [
   {
     key: "Access-Control-Allow-Origin",
-    value: "https://ackee.hikium.com",
+    value:
+      "https://ackee.hikium.com, https://ackee.hikium.com/api, https://*.hikium.com",
   },
   {
     key: "Access-Control-Allow-Methods",
@@ -20,6 +29,10 @@ const globalHeaders = [
   {
     key: "Access-Control-Max-Age",
     value: "3600",
+  },
+  {
+    key: "Content-Security-Policy",
+    value: contentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
   },
 ];
 

379cccf95763c313d1acd4275403c33b7da79f5e
Noah S <82749151+noahlst@users.noreply.github.com>
2023-03-07 19:52:35 +1100

    Remove old logo from Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
 // First party design
-import { Button, Card, Grid, GridContainer, Text } from "../components/design";
-
-// First party components
-import BrandLogo from "../components/brand/BrandLogo";
+import { Button, Grid, GridContainer, Text } from "../components/design";
 
 // Layout
 import Footer from "../components/layout/Footer";
@@ -16,45 +13,22 @@ export default function HomePage() {
           alignContent="center"
           direction="column"
         >
-          <Card>
-            <GridContainer gap={5} direction="column">
+          <Grid>
+            <GridContainer gap={1} direction="column">
               <Grid>
-                <div
-                  style={{
-                    backgroundColor: "#e9e1b0",
-                    padding: "2em",
-                    borderRadius: "1em",
-                    width: "8em",
-                  }}
-                >
-                  <div
-                    style={{
-                      width: "4em",
-                      height: "4em",
-                    }}
-                  >
-                    <BrandLogo />
-                  </div>
-                </div>
+                <Text h1>All-new Hikium Tempo.</Text>
               </Grid>
               <Grid>
-                <GridContainer gap={1} direction="column">
-                  <Grid>
-                    <Text h1>All-new Hikium Tempo.</Text>
-                  </Grid>
-                  <Grid>
-                    <Text>Start with a template. Make anything real.</Text>
-                  </Grid>
-                  <Grid>
-                    <Text>We're building it right now. More soon.</Text>
-                  </Grid>
-                  <Grid>
-                    <Button disabled>Coming Late 2023</Button>
-                  </Grid>
-                </GridContainer>
+                <Text>Start with a template. Make anything real.</Text>
+              </Grid>
+              <Grid>
+                <Text>We're building it right now. More soon.</Text>
+              </Grid>
+              <Grid>
+                <Button disabled>Coming Late 2023</Button>
               </Grid>
             </GridContainer>
-          </Card>
+          </Grid>
         </GridContainer>
       </Grid>
 

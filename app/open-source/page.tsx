0cdab595a4024b5834c7cf758f9924bad4dbe5aa
Noah S <82749151+noahlst@users.noreply.github.com>
2023-03-07 20:27:36 +1100

    Add more detail to open-source page

--- app/open-source/page.tsx
+++ app/open-source/page.tsx
@@ -18,35 +18,52 @@ export default function OpenSourcePage() {
   return (
     <>
       <Grid>
-        <Package size={36} />
-      </Grid>
-
-      <Grid>
-        <Text h1>Open-source at Hikium</Text>
-      </Grid>
-
-      <Grid>
-        <GridContainer gap={0} direction="column">
+        <GridContainer
+          justify="center"
+          alignContent="center"
+          direction="column"
+        >
           <Grid>
-            <Text>At Hikium, we love open-source.</Text>
-          </Grid>
-          <Grid>
-            <Text>
-              We built the{" "}
-              <GeistLink href="https://www.ulosino.com" icon>
-                ULOSINO
-              </GeistLink>{" "}
-              open-source operating system knowledge platform, the MXUPS
-              library, and so much more.
-            </Text>
+            <GridContainer gap={1} direction="column">
+              <Grid>
+                <Package size={48} />
+              </Grid>
+              <Grid>
+                <Text h1>We love open-source.</Text>
+              </Grid>
+              <Grid>
+                <Text>
+                  Building software that gives back to the community is the
+                  Hikium way.
+                </Text>
+              </Grid>
+              <Grid>
+                <Text>
+                  Open-source encourages and inspires young people to pursue
+                  careers in STEM fields. We're excited about how <br /> we can
+                  do this. We want to turn our ambition into action as soon as
+                  we can.
+                </Text>
+              </Grid>
+              <Grid>
+                <Text>
+                  That's why we're etching open-source communities into the
+                  fabric that makes Hikium Tempo. Because open-source <br />{" "}
+                  software powers our lives more and more, we want to excite
+                  generations present and emerging about what we share.
+                </Text>
+              </Grid>
+              <Grid>
+                <Text>Check out our GitHub to learn more.</Text>
+              </Grid>
+              <Grid>
+                <Link href="https://github.com/hikium" passHref>
+                  <Button>Our GitHub</Button>
+                </Link>
+              </Grid>
+            </GridContainer>
           </Grid>
         </GridContainer>
-      </Grid>
-
-      <Grid>
-        <Link href="https://github.com/hikium" passHref>
-          <Button>Our GitHub</Button>
-        </Link>
       </Grid>
 
       <Grid>
